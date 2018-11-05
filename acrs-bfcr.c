fc5ab7e237c0ea01c233f11dcad3d680d844a134
arthurcrs <acrs@cesar.school>
2018-11-05 14:24:48 -0300

    commit after class

--- acrs-bfcr.c
+++ acrs-bfcr.c
@@ -1,21 +1,22 @@
 /*
-Created by:
- Bernardo Russo 
- Arthur Carlos 
-
-Copyright © 2018 Russo Company. All rights reserved
-*/
+ Created by:
+ Bernardo Russo
+ Arthur Carlos
+ 
+ Copyright © 2018 Russo Company. All rights reserved
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAX_RANDOM_INT 50
+#define MAX_RANDOM_INT 500
 #define INSERTION_SORT 1
 #define BUBBLE_SORT 2
 #define SELECTION_SORT 3
 #define MERGE_SORT 4
 #define HEAP_SORT 5
-#define SIZE_OF_ARRAY 10
+#define SIZE_OF_ARRAY 30000
 
 //Insertion Sort
 void insertionSort(int *inputArray)
@@ -78,47 +79,62 @@ void selectionSort(int *inputArray)
 }
 
 //Merge Sort
-void merge(int *inputArray, int esq, int meio, int dir)
+void merge(int arr[], int l, int m, int r)
 {
-    int i = esq;
-    int j = meio + 1;
-    int count;
-    int aux[SIZE_OF_ARRAY];
-    for (count = i; count < SIZE_OF_ARRAY; count++)
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
     {
-        if (i >= meio + 1)
+        if (L[i] <= R[j])
         {
-            aux[count] = inputArray[j];
-            j++;
-        }
-        else if (j >= dir + 1)
-        {
-            aux[count] = inputArray[i];
+            arr[k] = L[i];
             i++;
         }
-        else if (inputArray[i] < inputArray[j])
+        else
         {
-            aux[count] = inputArray[i];
-            i++;
-        }
-        else if (inputArray[j] <= inputArray[i])
-        {
-            aux[count] = inputArray[j];
+            arr[k] = R[j];
             j++;
         }
+        k++;
     }
-    for (count = esq; count <= dir; count++)
-        inputArray[count] = aux[count];
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void mergeSort(int *inputArray, int esq, int dir)
+void mergeSort(int arr[], int l, int r)
 {
-    if (dir <= esq)
-        return;
-    int meio = (esq + dir) / 2;
-    mergeSort(inputArray, esq, meio);
-    mergeSort(inputArray, meio + 1, dir);
-    merge(inputArray, esq, meio, dir);
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
 }
 
 //Heap Sort
@@ -171,17 +187,6 @@ void heapSort(int *inputArray, int sizeOfArray)
 }
 
 //Base Code
-void printMenu()
-{
-    printf("Input the number corresponding to the sort you want to use:\n");
-    printf("1 : Insertion Sort \n");
-    printf("2 : Bubble Sort\n");
-    printf("3 : Selection rootIndexSort\n");
-    printf("4 : Merge Sort\n");
-    printf("5 : Heap Sort\n");
-    return;
-}
-
 int randInt()
 {
     return rand() % (MAX_RANDOM_INT + 1);
@@ -211,48 +216,72 @@ void initializeArray(int *inputArray)
     }
 }
 
-int main()
+void resetArray(int *array1, int *array2)
 {
-    int arrayToSort[SIZE_OF_ARRAY];
-    int choice;
-    char useAnotherSort = 'y';
-    while (useAnotherSort == 'y')
-    {
-        initializeArray(arrayToSort);
-        printf("The array of random int is: ");
-        printArray(arrayToSort);
-        printMenu();
-        printf("Enter your sort algorithm: ");
-        scanf("%d", &choice);
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+        array1[i] = array2[i];
+}
 
-        switch (choice)
-        {
-        case INSERTION_SORT:
-            insertionSort(arrayToSort);
-            break;
-        case BUBBLE_SORT:
-            bubbleSort(arrayToSort);
-            break;
-        case SELECTION_SORT:
-            selectionSort(arrayToSort);
-            break;
-        case MERGE_SORT:
-            mergeSort(arrayToSort, 0, SIZE_OF_ARRAY);
-            break;
-        case HEAP_SORT:
-            heapSort(arrayToSort, SIZE_OF_ARRAY);
-            break;
-        default:
-            printf("Invalid input \n");
-            printf("------------------------------------------------- \n");
-            continue;
-        }
-        printf("The array is now sorted: ");
-        printArray(arrayToSort);
-        printf("\nTest another sort algorithm? (y/n): ");
-        scanf(" %c", &useAnotherSort);
+void executeSort(int *inputArray, int sortAlgorithm)
+{
+
+    clock_t begin = clock();
+    char *algorithmName = "";
+    switch (sortAlgorithm)
+    {
+    case INSERTION_SORT:
+        insertionSort(inputArray);
+        algorithmName = "Insertion Sort";
+        break;
+    case BUBBLE_SORT:
+        bubbleSort(inputArray);
+        algorithmName = "Bubble Sort";
+        break;
+    case SELECTION_SORT:
+        selectionSort(inputArray);
+        algorithmName = "Selection Sort";
+        break;
+    case MERGE_SORT:
+        mergeSort(inputArray, 0, SIZE_OF_ARRAY);
+        algorithmName = "Merge Sort";
+        break;
+    case HEAP_SORT:
+        heapSort(inputArray, SIZE_OF_ARRAY);
+        algorithmName = "Heap Sort";
+        break;
+    default:
+        printf("Invalid input \n");
         printf("------------------------------------------------- \n");
     }
 
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("\nWith %s method the execution time was: %f", algorithmName, time_spent);
+}
+
+int main()
+{
+    int unsortedArray[SIZE_OF_ARRAY];
+    int arrayToSort[SIZE_OF_ARRAY];
+    initializeArray(unsortedArray);
+    printf("The array of random int is: ");
+    printArray(unsortedArray);
+
+    resetArray(arrayToSort, unsortedArray);
+    executeSort(arrayToSort, INSERTION_SORT);
+
+    resetArray(arrayToSort, unsortedArray);
+    executeSort(arrayToSort, BUBBLE_SORT);
+
+    resetArray(arrayToSort, unsortedArray);
+    executeSort(arrayToSort, SELECTION_SORT);
+
+    resetArray(arrayToSort, unsortedArray);
+    executeSort(arrayToSort, MERGE_SORT);
+
+    resetArray(arrayToSort, unsortedArray);
+    executeSort(arrayToSort, HEAP_SORT);
+
+    printf("\n\nWith an array size of %d\n", SIZE_OF_ARRAY);
     return 0;
 }
